f46dfaa0ba512834794e72c318b0dc051fac3e4b
ATOMIC <66838025+ATOMIC09@users.noreply.github.com>
2022-01-12 14:24:23 +0700

    Fix Bug ?

--- main.py
+++ main.py
@@ -1184,7 +1184,7 @@ async def on_member_join(person):
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name=bot.version))
-    gdrive_dl.download_file_from_google_drive("1rVl9NFS21ckBAD7tEYGrZkpHWtPZvtfy", "model")
+    gdrive_dl.download_file_from_google_drive("1rVl9NFS21ckBAD7tEYGrZkpHWtPZvtfy", "/model")
     print('Miura Started')
 
 @bot.event
